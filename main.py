08c06d9a7095ca3f7a5d3f9de409a5875062408f
Cristian Fernandez <130815136+cristianf18@users.noreply.github.com>
2023-09-17 00:55:12 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,65 +1,100 @@
-from flask import Flask, render_template, request, redirect, url_for
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>SongSinc</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link
+      rel="stylesheet"
+      href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+      integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"
+      crossorigin="anonymous"
+    />
+    <link
+      rel="stylesheet"
+      href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
+      crossorigin="anonymous"
+    />
+    <link rel="stylesheet" type="text/css" href="{{ url_for('static', filename='css/styles.css') }}">
+    <script src="{{ url_for('static', filename='js/script.js') }}"></script>
+  </head>
+  <body>
 
-app = Flask(__name__)
-
-import lyricsgenius
-import re
-import json
-import requests
-
-accessToken = "dBdNFFsh_ywi-VvikMkPjhpWsdH7kvHX_NLj4vDM07987oCDPwSpDIIIOflEsXX3"
-genius = lyricsgenius.Genius(accessToken)
-
-
-
-
-@app.route('/')
-def home():
-    return render_template('mainWebsite.html')
-
-
-
-@app.route('/processUserInput', methods=['POST'])
-def getInfo():
-    song = request.form.get('userInput')
-    genius_search_url = f"http://api.genius.com/search?q={song}&access_token={accessToken}"
-    responce = requests.get(genius_search_url)
-    json_data= responce.json()    
-    artistName = json_data["response"]["hits"][0]["result"]["artist_names"]
-
-    songName = json_data["response"]["hits"][0]["result"]["full_title"]
-    parts = songName.find("by")
-    songName = songName[:parts]
-    imageURL = json_data["response"]["hits"][0]["result"]["header_image_url"]
-
-    print(songName)
-    print(artistName)
-
-    return render_template('base.html', artistName=artistName, songName=songName, song=song, imageURL=imageURL)
-
-if __name__ == '__main__':
-    app.run(debug=True)
-"""
-
-@app.route('/processUserInput', methods=['POST'])
-def processUserInput():
-    userinput = request.form.get('userInput')
-    songName = genius.search_song(userinput)
-    artistName = genius.search_artist(userinput)
-    print(songName)
-    print(artistName)
+    <div class="topBar">
+      <img src="{{ url_for('static', filename='image/songsinctitle.png') }}" alt="please work" id='titleImage'>
+      <a href='https://genius.com/'>
+        <img src="{{ url_for('static', filename='image/geniuslogo.png') }}" alt="logo" id='logoImage' >
+      </a>
+      <div id='topSearch'>
+        <form>
+          <input type="text" name="lyrics" placeholder="Search lyrics">
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    </div>
     
-    print(songName)
-    if songName:
-        artistName = songName.artist
-        return render_template('base.html', songName=songName, songLyrics='These are the lyrics for "Nights."', userInput=userinput)
-    else:
-        errorMessage = f'Song "{userinput}" not found.'
-        return render_template('error.html', errorMessage=errorMessage)
 
+    <div class='blackBar'>
+      <span font-weight="normal" class="albums">ALBUMS</span>
+      <span font-weight="normal" class="songs">SONGS</span>
+      <span font-weight="normal" class="artists">ARTISTS</span>
+      <span font-weight="normal" class="charts">CHARTS</span>
+    </div>
+    <img src="{{ url_for('static', filename='image/oliviafront2.png') }}" alt="please work" class=overlay-image>
 
+    <div class='smallContainer'>
+      <div class="news">
+        <span class="newsHeader">NEWS</span>
+        <span class="horizontal-line"></span>
+      <h1>OLIVIA RODRIGO DROPPED "GUTS"</hi>
+        
+      </div>
+      <h1 class='liveText1'>
+        <span class="typing-text">Search for songs,</span>
+      <h1 class='liveText1'>
+        <span class="typing-text">artists, or</span>
+      <h1 class='liveText1'>
+        <span class="typing-text">albums</span>
+    </div>
 
+    <div class="biggerBlackBar">
+      <h1 class='searchText'>Search Below</h1>
+      <form method="POST" action="/processUserInput" class="invisible-input">
+        <input type="text" id="userInput" name="userInput" class = "invisibleInput">
+        <input type="submit" value="Submit">look</input>
+        
+      </form>   
+    </div>
 
+    
+    <div class='secondContainer'>
+      <div class='Box artistsBox' >
+        <h1 class='niceText text-container'>Artists</h1>
+          <p class='doja'>Doja Cat</p>
+      </div>
 
+      <div class='Box chartsBox'>
+        <h1 class='niceText text-container'>Charts</h1>
+        <div id="song_link" data-song-name="Nights">
+          <p class='doja'>Nights</p>
+        </div>
+      </div>
 
-"""
+      <div class='Box songsBox'>
+        <h1 class='niceText  text-container'>Songs</h1>
+        <p class='doja'>deja vu</p>
+      </div>
+
+      <div class="Box albumsBox">
+        <h1 class="niceText text-container">Albums</h1>
+        <p class='doja'>Blonde</p>
+      </div>
+    
+    
+
+    {% block content %}
+    
+    {% endblock %}
+    
+  </body>
+</html>
